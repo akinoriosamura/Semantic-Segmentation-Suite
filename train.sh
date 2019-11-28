8a05a37d24ed0093e6437914b8adb0a27d243a0d
akinoriosamura <osamura.akinori@gmail.com>
2019-11-28 10:01:34 +0000

    update train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,6 @@
-python train.py --dataset=CamVid/ --batch_size=1 --crop_or_resize=crop
-# python train.py --dataset=CelebAMask-HQ-skin-eye-lips/ --batch_size=1 --crop_or_resize=resize
+# python train.py --dataset=CamVid/ --batch_size=1 --crop_or_resize=crop
+python train.py --dataset=CelebAMask-HQ-skin-eye-lips/ \
+	--batch_size=1 \
+	--crop_or_resize=resize \
+	--model=MobileUNet-Skip \
+	--frontend=MobileNetV2
